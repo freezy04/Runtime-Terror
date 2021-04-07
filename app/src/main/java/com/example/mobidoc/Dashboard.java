81682274a8bc9612ed70e07dcceceb34fface825
Dylan-Fasser <2179115@students.wits.ac.za>
2021-04-07 22:05:14 +0200

    Added a User Dashboard Page as a means of redirection

---
+++ app/src/main/java/com/example/mobidoc/Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.mobidoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
