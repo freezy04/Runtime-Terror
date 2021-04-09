2452ab37095d6d4df298d828a93421585fb11c29
Dylan-Fasser <2179115@students.wits.ac.za>
2021-04-09 08:15:08 +0200

    Improved Functionalities : See Summary Below
    Added new utilities for the Login Page , Created a Tool bar to allow the user to sign / log out of the app and added an image to each dashboard to look less empty

---
+++ app/src/main/java/com/example/mobidoc/Doctor_Dashboard.java
@@ -0,0 +1,41 @@
+package com.example.mobidoc;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import io.paperdb.Paper;
+
+public class Doctor_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor__dashboard);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.log_out:
+                Paper.book().delete(Utilities.USER_KEY);
+                startActivity(new Intent(Doctor_Dashboard.this , MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/mobidoc/Utilities.java
@@ -0,0 +1,20 @@
+package com.example.mobidoc;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Utilities {
+
+        public static final String USER_KEY = "default";
+        public static final String Patient = "patient";
+        public static final String Doctor = "doctor";
+
+
+
+
+}
