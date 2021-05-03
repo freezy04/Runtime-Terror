fcdc2e746511f6274ed4289ff8649f71d62eb5d8
Neo Moeketsi <1936996@students.wits.ac.za>
2021-05-03 02:56:05 +0200

    Doctors can edit and update their profiles

---
+++ app/src/main/java/com/example/mobidoc/ui/registration/Doctor_ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.mobidoc.ui.registration;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobidoc.R;
+
+public class Doctor_ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor__profile);
+    }
+}
