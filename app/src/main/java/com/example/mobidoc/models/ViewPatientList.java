047fcab52d97964867710574a3b3bb476824febe
1866305 <1866305@students.wits.ac.za>
2021-05-04 13:06:06 +0200

    Monior commit (Patient checkout

---
+++ app/src/main/java/com/example/mobidoc/models/ViewPatientList.java
@@ -0,0 +1,16 @@
+package com.example.mobidoc.models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobidoc.R;
+
+public class ViewPatientList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_patient_list);
+    }
+}
