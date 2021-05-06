1fda4b23493e72fa0780618424fd7a192ca7027b
1866305 <1866305@students.wits.ac.za>
2021-05-06 08:08:44 +0200

    Updated BookedAppointmentList

--- app/src/main/java/com/example/mobidoc/models/BookedAppointmentList.java
+++ app/src/main/java/com/example/mobidoc/models/BookedAppointmentList.java
@@ -1,50 +1,71 @@
 package com.example.mobidoc.models;
 
-import com.google.firebase.database.Query;
-
 /**
  * Created by Neo on 24-Apr-21.
  */
 
 public class BookedAppointmentList {
 
-    private String Date;
-    private String Time;
-    private String Doctor_ID;
+
+    private String Time_for_appointment;
+    private String DoctorUid;
     private String PatientID;
+    private String Patient_Message;
+    private String Patient_Name;
+    private String Date_for_appointment;
 
     public BookedAppointmentList() {
     }
 
-    public BookedAppointmentList(String date, String time, String doctor_ID, String patientID) {
-        this.Date = date;
-        this.Time = time;
-        this.Doctor_ID = doctor_ID;
+    public BookedAppointmentList( String DoctorUid,String Date_for_appointment, String Patient_Message,String Patient_Name, String Time_for_appointment, String patientID) {
+        this.Date_for_appointment =Date_for_appointment;
+        this.Time_for_appointment = Time_for_appointment;
+        this.DoctorUid = DoctorUid;
         this.PatientID = patientID;
+        this.Patient_Name = Patient_Name;
+        this.Patient_Message = Patient_Message;
     }
 
-    public String getDate() {
-        return Date;
+
+
+
+
+    public String getPatient_Message() {
+        return Patient_Message;
+    }
+    public void setPatient_Message(String Patient_Message) {
+        this.Patient_Message = Patient_Message;
     }
 
-    public void setDate(String date) {
-        this.Date = date;
+    public String getPatient_Name() {
+        return Patient_Name;
+    }
+    public void setPatient_Name(String Patient_Name) {
+        this.Patient_Name = Patient_Name;
     }
 
-    public String getTime() {
-        return Time;
+    public String getDate_for_appointment() {
+        return Date_for_appointment;
     }
 
-    public void setTime(String time) {
-        this.Time = time;
+    public void setDate_for_appointment(String Date_for_appointment) {
+        this.Date_for_appointment =Date_for_appointment;
     }
 
-    public String getDoctor_ID() {
-        return Doctor_ID;
+    public String getTime_for_appointment() {
+        return Time_for_appointment;
     }
 
-    public void setDoctor_ID(String doctor_ID) {
-        this.Doctor_ID = doctor_ID;
+    public void setTime_for_appointment(String time_for_appointment) {
+        this.Time_for_appointment = time_for_appointment;
+    }
+
+    public String getDoctorUid() {
+        return DoctorUid;
+    }
+
+    public void setDoctorUid(String DoctorUid) {
+        this.DoctorUid= DoctorUid;
     }
 
     public String getPatientID() {
