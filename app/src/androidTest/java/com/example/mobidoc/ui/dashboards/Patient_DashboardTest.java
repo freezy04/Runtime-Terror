d3063fbbd001d869399c360d6a659df287370552
Kgosi Gabaocwe <2135594@students.wits.ac.za>
2021-05-23 19:04:51 +0200

    Updated login details for test

--- app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
@@ -2,10 +2,16 @@ package com.example.mobidoc.ui.dashboards;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import com.example.mobidoc.adapters.adapterPatientUpcoming;
+import com.example.mobidoc.models.Appointment;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Patient_DashboardTest {
     @Rule
@@ -14,22 +20,12 @@ public class Patient_DashboardTest {
 
     @Before
     public void LoginUser(){
-        Thread t = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                    firebaseAuth.signInWithEmailAndPassword("bm@gmail.com","Jazzman@1");
-                    synchronized(this){
-                        wait(3000);
-                    }
-                } catch(InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        t.start();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword("iv@email.com","Password1!");
     }
+    @Test
+    public void testInUserAcceptanceCriteria(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
 
+        });
+    }
 }
