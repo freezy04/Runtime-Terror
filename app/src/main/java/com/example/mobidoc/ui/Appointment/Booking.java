b76a3d2d2594e1f464ce4de5c141c1ee80e23840
kgosi-gabaocwe <2135594@students.wits.ac.za>
2021-05-23 21:27:03 +0200

    restructured function

--- app/src/main/java/com/example/mobidoc/ui/Appointment/Booking.java
+++ app/src/main/java/com/example/mobidoc/ui/Appointment/Booking.java
@@ -283,29 +283,33 @@ public class Booking extends AppCompatActivity {
     public void ClickNavBar() {
         home_nav = findViewById(R.id.bottom_navigation);
         home_nav.setSelectedItemId(R.id.menu_consultation);
-        home_nav.setOnNavigationItemSelectedListener(item -> {
-            Intent activity;
+        home_nav.setOnNavigationItemSelectedListener(item -> onNavBarItemClicked(item.getItemId()));
+    }
 
-            switch (item.getItemId()) {
+    public boolean onNavBarItemClicked(int itemId){
+        Intent activity;
 
-                case R.id.menu_home:
-                    activity = new Intent(Booking.this, Patient_Dashboard.class);
-                    startActivity(activity);
-                    return true;
-                case R.id.menu_appointments:
-                    activity = new Intent(Booking.this, DoctorViewAcceptedAppointmentsActivity.class);
-                    startActivity(activity);
-                    break;
-                case R.id.menu_consultation:
-                    return true;
-                case R.id.menu_profile:
-                    activity = new Intent(Booking.this, Patient_Profile.class);
-                    startActivity(activity);
-                    return true;
+        switch (itemId) {
 
-            }
-            return true;
+            case R.id.menu_home:
+                activity = new Intent(Booking.this, Patient_Dashboard.class);
+                startActivity(activity);
+                return true;
 
-        });
+            case R.id.menu_appointments:
+                activity = new Intent(Booking.this, DoctorViewAcceptedAppointmentsActivity.class);
+                startActivity(activity);
+                break;
+
+            case R.id.menu_consultation:
+                return true;
+
+            case R.id.menu_profile:
+                activity = new Intent(Booking.this, Patient_Profile.class);
+                startActivity(activity);
+                return true;
+
+        }
+        return true;
     }
 }
