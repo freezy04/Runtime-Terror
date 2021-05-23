84544e70a2881cdb40738869f5b2de9ef202d927
Dylan-Fasser <2179115@students.wits.ac.za>
2021-05-23 23:12:31 +0200

    Added unit Tests for the Patient Dashboard

--- app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
@@ -2,6 +2,7 @@ package com.example.mobidoc.ui.dashboards;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import com.example.mobidoc.R;
 import com.example.mobidoc.adapters.adapterPatientUpcoming;
 import com.example.mobidoc.models.Appointment;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +27,34 @@ public class Patient_DashboardTest {
     public void testInUserAcceptanceCriteria(){
         activityScenarioRule.getScenario().onActivity(activity -> {
 //
+        });
+    }
+
+    @Test
+    public void clickOnNavBar_consultation() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.menu_consultation);
+        });
+    }
+
+    @Test
+    public void clickOnNavBar_appointment() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.menu_appointments);
+        });
+    }
+
+    @Test
+    public void clickOnNavBar_profle() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.menu_profile);
+        });
+    }
+
+    @Test
+    public void clickOnNavBar_home() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.menu_home);
         });
     }
 }
