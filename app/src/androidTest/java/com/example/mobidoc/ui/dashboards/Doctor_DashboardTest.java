9a860b4177b2ce9702614f97844112d8d450c09f
gabe <gabelancaster0@gmail.com>
2021-06-03 18:06:52 +0200

    Deleted unused file

--- app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Doctor_DashboardTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Doctor_DashboardTest.java
@@ -3,7 +3,6 @@ package com.example.mobidoc.ui.dashboards;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import com.example.mobidoc.R;
-import com.example.mobidoc.adapters.adapterPatientUpcoming;
 import com.example.mobidoc.models.Appointment;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -23,16 +22,16 @@ public class Doctor_DashboardTest {
     public void loginUser(){
         FirebaseAuth.getInstance().signInWithEmailAndPassword("rr@email.com","Password1!");
     }
-    @Test
-    public void testInUserAcceptanceCriteria(){
-        activityScenarioRule.getScenario().onActivity(activity -> {
-            //activityScenarioRule.getScenario().moveToState(Lifecycle.State.CREATED);
-           // Bundle b = new Bundle();
-           // activity.onCreate(b);
-            List<Appointment> userList = new ArrayList<>();
-            adapterPatientUpcoming A = new adapterPatientUpcoming(activity,userList);
-        });
-    }
+//    @Test
+//    public void testInUserAcceptanceCriteria(){
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            //activityScenarioRule.getScenario().moveToState(Lifecycle.State.CREATED);
+//           // Bundle b = new Bundle();
+//           // activity.onCreate(b);
+//            List<Appointment> userList = new ArrayList<>();
+//            adapterPatientUpcoming A = new adapterPatientUpcoming(activity,userList);
+//        });
+//    }
 
     @Test
     public void getAllUsers_(){
