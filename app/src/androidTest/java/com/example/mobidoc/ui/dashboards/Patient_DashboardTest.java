e75b382e1ec37a9bf5e32fe793690d2edd9959b7
freezy04 <69632515+freezy04@users.noreply.github.com>
2021-06-05 15:13:06 +0200

    patient Dashboard

--- app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/dashboards/Patient_DashboardTest.java
@@ -1,59 +1,59 @@
-//package com.example.mobidoc.ui.dashboards;
+package com.example.mobidoc.ui.dashboards;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.example.mobidoc.R;
+import com.example.mobidoc.adapters.adapterPatientUpcoming;
+import com.example.mobidoc.models.Appointment;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient_DashboardTest {
+//    @Rule
+//    public ActivityScenarioRule<Patient_Dashboard> activityScenarioRule = new ActivityScenarioRule<>(Patient_Dashboard.class);
+////   @Before
+//   public void loginUser(){
+//       FirebaseAuth.getInstance().signInWithEmailAndPassword("iv@email.com","Password1!");
+//   }
 //
-//import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//   @Test
+//   public void testInUserAcceptanceCriteria(){
+//      activityScenarioRule.getScenario().onActivity(activity -> {
 //
-//import com.example.mobidoc.R;
-//import com.example.mobidoc.adapters.adapterPatientUpcoming;
-//import com.example.mobidoc.models.Appointment;
-//import com.google.firebase.auth.FirebaseAuth;
+//       });
+//    }
 //
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
+//    @Test
+//    public void clickOnNavBar_consultation() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_consultation);
+//        });
+//    }
 //
-//import java.util.ArrayList;
-//import java.util.List;
+//    @Test
+//    public void clickOnNavBar_appointment() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_appointments);
+//        });
+//    }
 //
-//public class Patient_DashboardTest {
-////    @Rule
-////    public ActivityScenarioRule<Patient_Dashboard> activityScenarioRule = new ActivityScenarioRule<>(Patient_Dashboard.class);
-//////   @Before
-////   public void loginUser(){
-////       FirebaseAuth.getInstance().signInWithEmailAndPassword("iv@email.com","Password1!");
-////   }
-////
-////   @Test
-////   public void testInUserAcceptanceCriteria(){
-////      activityScenarioRule.getScenario().onActivity(activity -> {
-////
-////       });
-////    }
-////
-////    @Test
-////    public void clickOnNavBar_consultation() {
-////        activityScenarioRule.getScenario().onActivity(activity -> {
-////            activity.ClickOnNavBar(R.id.menu_consultation);
-////        });
-////    }
-////
-////    @Test
-////    public void clickOnNavBar_appointment() {
-////        activityScenarioRule.getScenario().onActivity(activity -> {
-////            activity.ClickOnNavBar(R.id.menu_appointments);
-////        });
-////    }
-////
-////    @Test
-////    public void clickOnNavBar_profle() {
-////        activityScenarioRule.getScenario().onActivity(activity -> {
-////            activity.ClickOnNavBar(R.id.menu_profile);
-////        });
-////    }
-////
-////    @Test
-////    public void clickOnNavBar_home() {
-////        activityScenarioRule.getScenario().onActivity(activity -> {
-////            activity.ClickOnNavBar(R.id.menu_home);
-////        });
-////    }
-//}
+//    @Test
+//    public void clickOnNavBar_profle() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_profile);
+//        });
+//    }
+//
+//    @Test
+//    public void clickOnNavBar_home() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_home);
+//        });
+//    }
+}
