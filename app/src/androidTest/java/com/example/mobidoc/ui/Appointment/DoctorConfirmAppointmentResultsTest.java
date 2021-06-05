fb58368af6eead90966ea07a970f35f9d0de6793
1866305 <69643483+1866305@users.noreply.github.com>
2021-06-05 15:15:53 +0200

    Update DoctorConfirmAppointmentResultsTest.java
    Uncommented DoctorConfirmAppointment

--- app/src/androidTest/java/com/example/mobidoc/ui/Appointment/DoctorConfirmAppointmentResultsTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/Appointment/DoctorConfirmAppointmentResultsTest.java
@@ -1,57 +1,57 @@
-// package com.example.mobidoc.ui.Appointment;
+package com.example.mobidoc.ui.Appointment;
 
-// import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
-// import com.google.firebase.auth.FirebaseAuth;
-// import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-// import org.junit.Before;
-// import org.junit.Rule;
-// import org.junit.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
-// public class DoctorConfirmAppointmentResultsTest {
+public class DoctorConfirmAppointmentResultsTest {
 
-//     @Rule
-//     public ActivityScenarioRule<DoctorConfirmAppointmentResults> activityScenarioRule = new ActivityScenarioRule<>(DoctorConfirmAppointmentResults.class);
+    @Rule
+    public ActivityScenarioRule<DoctorConfirmAppointmentResults> activityScenarioRule = new ActivityScenarioRule<>(DoctorConfirmAppointmentResults.class);
 
-// //    @Before
-// //    public void loginUser() {
-// //
-// //        FirebaseAuth.getInstance().signInWithEmailAndPassword("Test2@gmail.com", "Freezy@04");
-// //        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-// //    }
+   @Before
+   public void loginUser() {
 
-//     @Test
-//     public void updateDetails_UpdatesConfirmed_UpdateUnsuccessful(){
-//         activityScenarioRule.getScenario().onActivity(activity -> activity.confirmBTN.performClick());
-//     }
+       FirebaseAuth.getInstance().signInWithEmailAndPassword("Test2@gmail.com", "Freezy@04");
+       FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+   }
 
-//     @Test
-//     public void initializationActivityTest(){
-//         activityScenarioRule.getScenario().onActivity(activity -> {
-//                 activity.initializeActivity_();
-//     });
-//     }
-// //  consult
-// //    @Test
-// //    public void ConfirmUpdateTest(){
-// //        activityScenarioRule.getScenario().onActivity(activity -> {
-// //            activity.confirmUpdates_("panado","50","1 per day");
-// //        });
-// //    }
+    @Test
+    public void updateDetails_UpdatesConfirmed_UpdateUnsuccessful(){
+        activityScenarioRule.getScenario().onActivity(activity -> activity.confirmBTN.performClick());
+    }
 
-//     @Test
-//     public void updateDetails_Unsecceful(){
-//         activityScenarioRule.getScenario().onActivity(activity -> {
-//             activity.updateDetails_();
-//         });
-//     }
+    @Test
+    public void initializationActivityTest(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+                activity.initializeActivity_();
+    });
+    }
+//  consult
+   @Test
+   public void ConfirmUpdateTest(){
+       activityScenarioRule.getScenario().onActivity(activity -> {
+           activity.confirmUpdates_("panado","50","1 per day");
+       });
+   }
 
-// //    @Test
-// //    public void onSupportNavigateUpTest(){
-// //        activityScenarioRule.getScenario().onActivity(activity -> {
-// //           activity.onSupportNavigateUp();
-// //        });
-// //    }
+    @Test
+    public void updateDetails_Unsecceful(){
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.updateDetails_();
+        });
+    }
 
-// }
+   @Test
+   public void onSupportNavigateUpTest(){
+       activityScenarioRule.getScenario().onActivity(activity -> {
+          activity.onSupportNavigateUp();
+       });
+    }
+
+}
