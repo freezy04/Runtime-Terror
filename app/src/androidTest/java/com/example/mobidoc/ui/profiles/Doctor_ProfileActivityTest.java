57e3def7d74022532867fc3c9208edec257a7b4b
1866305 <69643483+1866305@users.noreply.github.com>
2021-06-06 04:11:00 +0200

    Update Doctor_ProfileActivityTest.java
    added unit tests

--- app/src/androidTest/java/com/example/mobidoc/ui/profiles/Doctor_ProfileActivityTest.java
+++ app/src/androidTest/java/com/example/mobidoc/ui/profiles/Doctor_ProfileActivityTest.java
@@ -19,32 +19,82 @@ public class Doctor_ProfileActivityTest {
           activity.getDoctorDetails("JAKLyVL37YV3PM0HZoUa8EhFWth2");
        });
    }
+    @Test
+    public void clickHomeNavBarItem() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.nav_home2);
+        });
+    }
 
-   @Test
-   public void clickOnNavBar_consultation() {
-       activityScenarioRule.getScenario().onActivity(activity -> {
-           activity.ClickOnNavBar(R.id.menu_consultation);
-       });
-   }
+    @Test
+    public void clickPatientRecordsNavBarItem() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar( R.id.nav_pateintrecords);
+        });
 
-   @Test
-   public void clickOnNavBar_appointment() {
-       activityScenarioRule.getScenario().onActivity(activity -> {
-           activity.ClickOnNavBar(R.id.menu_appointments);
-       });
-   }
+    }
 
-   @Test
-   public void clickOnNavBar_profle() {
-       activityScenarioRule.getScenario().onActivity(activity -> {
-           activity.ClickOnNavBar(R.id.menu_profile);
-       });
-   }
+    @Test
+    public void clickProfilenNavBarItem() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.nav_profile2);
+        });
 
-   @Test
-   public void clickOnNavBar_home() {
-       activityScenarioRule.getScenario().onActivity(activity -> {
-           activity.ClickOnNavBar(R.id.menu_home);
-       });
-   }
+    }
+
+    @Test
+    public void clickPendingTest() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar(R.id.nav_pendingappointments2);
+        });
+    }
+    @Test
+    public void clickAcceptanceTestNavBarItem2() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            activity.ClickOnNavBar( R.id.nav_acceptedappointments2);
+        });
+    }
+
+
+
+
+
+ @Test
+       public void onBackPressed() {
+
+               activityScenarioRule.getScenario().onActivity(activity -> {
+
+                       activity.onSupportNavigateUp();
+                       activity.onBackPressed();
+
+               });
+       }
+
+//    @Test
+//    public void clickOnNavBar_consultation() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_consultation);
+//        });
+//    }
+
+//    @Test
+//    public void clickOnNavBar_appointment() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_appointments);
+//        });
+//    }
+
+//    @Test
+//    public void clickOnNavBar_profle() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_profile);
+//        });
+//    }
+
+//    @Test
+//    public void clickOnNavBar_home() {
+//        activityScenarioRule.getScenario().onActivity(activity -> {
+//            activity.ClickOnNavBar(R.id.menu_home);
+//        });
+//    }
 }
